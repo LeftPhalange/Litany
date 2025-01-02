02961c30218715a719e7f48775bec00ba443e790
Ethan Bovard <ethanbovard@hotmail.com>
2025-01-01 20:21:04 -0500

    Add Latin subset for Inter in src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({
-  variable: "--font-inter"
+  variable: "--font-inter",
+  subsets: ["latin"]
 });
 export const metadata: Metadata = {
   title: "Litany",
